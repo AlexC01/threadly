2685af70858aa5249c49b6dbbe53c8328b957a9b
AlexC01 <acuadraq@gmail.com>
2025-09-08 15:22:53 -0400

    feat: creating the homepage description: adding the title and threads to the homepage with a sort function

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,27 @@
+import Threads from "@/components/Home/Threads";
+import { Button } from "@/components/ui/button";
+
 export default function Home() {
-	return <div>xd</div>;
+	return (
+		<main className="min-h-screen">
+			<div className="container mx-auto py-12 md:py-16">
+				<div className="text-center py-12 px-4">
+					<h1 className="text-5xl font-bold md:text-7xl tracking-tighter">
+						Join the Conversation
+					</h1>
+					<h2 className="text-2xl text-muted-foreground mt-5 max-w-2xl mx-auto">
+						A modern board for open and anonymous discussions.
+					</h2>
+				</div>
+				<div className="flex justify-center px-4">
+					<Button variant="default" className="font-bold px-12 text-xl py-8">
+						Create Thread
+					</Button>
+				</div>
+				<div className="mt-10 px-4 md:px-8">
+					<Threads />
+				</div>
+			</div>
+		</main>
+	);
 }
---
+++ src/components/Home/Threads.tsx
@@ -0,0 +1,33 @@
+"use client";
+
+import { useState } from "react";
+import ThreadCard from "../Threads/ThreadCard";
+
+const Threads = () => {
+	const [sort, setSort] = useState("top");
+	const sortOptions = ["top", "new", "hot"];
+	return (
+		<>
+			<div className="bg-muted text-muted-foreground inline-flex flex-row space-x-3 px-3 py-3 rounded-md">
+			{sortOptions.map((option) => (
+          <button
+            key={option}
+            onClick={() => setSort(option)}
+            type="button"
+            className={`${
+              sort === option ? "bg-background shadow-md" : ""
+            } px-4 py-2 rounded-sm capitalize font-semibold transition-colors hover:bg-background/80 cursor-pointer`}
+          >
+            {option}
+          </button>
+        ))}
+			</div>
+			<section className="mt-10 grid grid-cols-1 lg:grid-cols-2 gap-8">
+				<ThreadCard />
+				<ThreadCard />
+			</section>
+		</>
+	);
+};
+
+export default Threads;
