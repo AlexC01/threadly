10595a1e9d4b6f4d563a15651df542a201c03e94
AlexC01 <acuadraq@gmail.com>
2025-09-09 13:02:44 -0400

    feat: finishing the homepage

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,8 @@
+import { ShieldCheck } from "lucide-react";
+import Link from "next/link";
 import Threads from "@/components/Home/Threads";
 import { Button } from "@/components/ui/button";
-import { Card } from "@/components/ui/card";
+import { Card, CardContent, CardFooter, CardTitle } from "@/components/ui/card";
 
 export default function Home() {
 	return (
@@ -22,7 +24,13 @@ export default function Home() {
 				<div className="mt-10 px-4 md:px-8">
 					<Threads />
 				</div>
-				<div className="mt-16 px-4 md:px-8 max-w-2xl mx-auto">
+				<div className="mt-16 text-center">
+					<h2 className="text-4xl font-bold tracking-tight">Get Involved</h2>
+					<p className="text-muted-foreground mt-4 text-lg">
+						Discover the benefits of joining our community.
+					</p>
+				</div>
+				<div className="mt-10 md:mt-16 px-4 md:px-8 gap-8 grid grid-cols-1 sm:grid-cols-2">
 					<Card className="px-8">
 						<h3 className="text-2xl font-bold">Why Sign In?</h3>
 						<ul className="list-disc [&>li]:mt-2 -mt-2">
@@ -33,6 +41,21 @@ export default function Home() {
 						<Button className="mt-2" variant="outline">
 							Log In / Sign Up
 						</Button>
+					</Card>
+					<Card className="flex flex-col items-center text-center">
+						<ShieldCheck className="h-12 w-12 mx-auto text-primary" />
+						<CardTitle className="text-xl">Community Guidelines</CardTitle>
+						<CardContent>
+							<p className="text-sm text-muted-foreground">
+								A few simple rules to help keep Threadly a respectful and
+								engaging place for everyone.
+							</p>
+						</CardContent>
+						<CardFooter className="mx-auto">
+							<Button asChild className="w-full">
+								<Link href="/">Learn More</Link>
+							</Button>
+						</CardFooter>
 					</Card>
 				</div>
 			</div>
