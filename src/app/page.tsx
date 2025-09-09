9f230ea9478c158c2eba2aee8b8ca61c01f07987
AlexC01 <acuadraq@gmail.com>
2025-09-09 09:56:27 -0400

    feat: adding another section to the homepage

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,11 @@
 import Threads from "@/components/Home/Threads";
 import { Button } from "@/components/ui/button";
+import { Card } from "@/components/ui/card";
 
 export default function Home() {
 	return (
 		<main className="min-h-screen">
-			<div className="container mx-auto py-12 md:py-16">
+			<div className="max-w-7xl mx-auto py-12 md:py-16">
 				<div className="text-center py-12 px-4">
 					<h1 className="text-5xl font-bold md:text-7xl tracking-tighter">
 						Join the Conversation
@@ -20,6 +21,19 @@ export default function Home() {
 				</div>
 				<div className="mt-10 px-4 md:px-8">
 					<Threads />
+				</div>
+				<div className="mt-16 px-4 md:px-8 max-w-2xl mx-auto">
+					<Card className="px-8">
+						<h3 className="text-2xl font-bold">Why Sign In?</h3>
+						<ul className="list-disc [&>li]:mt-2 -mt-2">
+							<li>Bookmark favorite threads</li>
+							<li>Manage your own posts</li>
+							<li>Get a persistent anonymous ID</li>
+						</ul>
+						<Button className="mt-2" variant="outline">
+							Log In / Sign Up
+						</Button>
+					</Card>
 				</div>
 			</div>
 		</main>
