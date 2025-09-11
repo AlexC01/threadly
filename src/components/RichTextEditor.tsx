b2e02a585e9b175a2e093a0dca6b5e0a0651750e
AlexC01 <acuadraq@gmail.com>
2025-09-11 13:29:51 -0400

    feat: finishing the richtext editor

--- src/components/RichTextEditor.tsx
+++ src/components/RichTextEditor.tsx
@@ -2,10 +2,20 @@
 
 "use client";
 
+import CharacterCount from "@tiptap/extension-character-count";
 import { type Editor, EditorContent, useEditor } from "@tiptap/react";
 import StarterKit from "@tiptap/starter-kit";
-import CharacterCount from '@tiptap/extension-character-count';
-import { Bold, Italic, Strikethrough } from "lucide-react";
+import {
+	Bold,
+	Code,
+	Italic,
+	List,
+	ListOrdered,
+	SquareTerminal,
+	Strikethrough,
+	TextQuote,
+	Undo,
+} from "lucide-react";
 import { Toggle } from "@/components/ui/toggle";
 
 interface ToolbarProps {
@@ -43,6 +53,86 @@ const Toolbar = ({ editor }: ToolbarProps) => {
 			>
 				<Strikethrough className="h-4 w-4" />
 			</Toggle>
+			<Toggle
+				className="cursor-pointer"
+				size="sm"
+				pressed={editor.isActive("code")}
+				onPressedChange={() => editor.chain().focus().toggleCode().run()}
+			>
+				<Code className="h-4 w-4" />
+			</Toggle>
+			<Toggle
+				className="cursor-pointer"
+				size="sm"
+				pressed={editor.isActive("codeBlock")}
+				onPressedChange={() => editor.chain().focus().toggleCodeBlock().run()}
+			>
+				<SquareTerminal />
+			</Toggle>
+			<Toggle
+				className="cursor-pointer"
+				size="sm"
+				pressed={editor.isActive("heading", { level: 1 })}
+				onPressedChange={() =>
+					editor.chain().focus().toggleHeading({ level: 1 }).run()
+				}
+			>
+				H1
+			</Toggle>{" "}
+			<Toggle
+				className="cursor-pointer"
+				size="sm"
+				pressed={editor.isActive("heading", { level: 2 })}
+				onPressedChange={() =>
+					editor.chain().focus().toggleHeading({ level: 2 }).run()
+				}
+			>
+				H2
+			</Toggle>
+			<Toggle
+				className="cursor-pointer"
+				size="sm"
+				pressed={editor.isActive("heading", { level: 3 })}
+				onPressedChange={() =>
+					editor.chain().focus().toggleHeading({ level: 3 }).run()
+				}
+			>
+				H3
+			</Toggle>
+			<Toggle
+				className="cursor-pointer"
+				size="sm"
+				pressed={editor.isActive("heading", { level: 4 })}
+				onPressedChange={() =>
+					editor.chain().focus().toggleHeading({ level: 4 }).run()
+				}
+			>
+				H4
+			</Toggle>
+			<Toggle
+				className="cursor-pointer"
+				size="sm"
+				pressed={editor.isActive("bulletList")}
+				onPressedChange={() => editor.chain().focus().toggleBulletList().run()}
+			>
+				<List className="h-4 w-4" />
+			</Toggle>
+			<Toggle
+				className="cursor-pointer"
+				size="sm"
+				pressed={editor.isActive("orderedList")}
+				onPressedChange={() => editor.chain().focus().toggleOrderedList().run()}
+			>
+				<ListOrdered className="h-4 w-4" />
+			</Toggle>
+			<Toggle
+				className="cursor-pointer"
+				size="sm"
+				pressed={editor.isActive("blockquote")}
+				onPressedChange={() => editor.chain().focus().toggleBlockquote().run()}
+			>
+				<TextQuote className="h-4 w-4" />
+			</Toggle>
 		</div>
 	);
 };
@@ -53,10 +143,14 @@ interface TipTapEditorProps {
 	limit: number;
 }
 
-export const TiptapEditor = ({ onChange, content, limit }: TipTapEditorProps) => {
+export const TiptapEditor = ({
+	onChange,
+	content,
+	limit,
+}: TipTapEditorProps) => {
 	// The "Brain"
 	const editor = useEditor({
-		extensions: [StarterKit.configure(), CharacterCount.configure({limit})],
+		extensions: [StarterKit.configure(), CharacterCount.configure({ limit })],
 		immediatelyRender: false,
 		editorProps: {
 			attributes: {
