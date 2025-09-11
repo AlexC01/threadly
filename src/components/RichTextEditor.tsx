f78285f2e60bcf2107f597e6c4afff8969bbcd6d
AlexC01 <acuadraq@gmail.com>
2025-09-10 22:05:24 -0400

    feat: creating component for the richeditor

---
+++ src/components/RichTextEditor.tsx
@@ -0,0 +1,76 @@
+// in components/RichTextEditor.tsx
+
+"use client";
+
+import { type Editor, EditorContent, useEditor } from "@tiptap/react";
+import StarterKit from "@tiptap/starter-kit";
+import { Bold, Italic, Strikethrough } from "lucide-react";
+import { Toggle } from "@/components/ui/toggle";
+
+interface ToolbarProps {
+	editor: Editor | null;
+}
+
+// The "Controls"
+const Toolbar = ({ editor }: ToolbarProps) => {
+	if (!editor) {
+		return null;
+	}
+	return (
+		<div className="border border-input bg-transparent rounded-md p-1 flex gap-1">
+			<Toggle
+				size="sm"
+				className="cursor-pointer"
+				pressed={editor.isActive("bold")}
+				onPressedChange={() => editor.chain().focus().toggleBold().run()}
+			>
+				<Bold className="h-4 w-4" />
+			</Toggle>
+			<Toggle
+				className="cursor-pointer"
+				size="sm"
+				pressed={editor.isActive("italic")}
+				onPressedChange={() => editor.chain().focus().toggleItalic().run()}
+			>
+				<Italic className="h-4 w-4" />
+			</Toggle>
+			<Toggle
+				className="cursor-pointer"
+				size="sm"
+				pressed={editor.isActive("strike")}
+				onPressedChange={() => editor.chain().focus().toggleStrike().run()}
+			>
+				<Strikethrough className="h-4 w-4" />
+			</Toggle>
+		</div>
+	);
+};
+
+interface TipTapEditorProps {
+	onChange: (richText: string) => void;
+	content: string;
+}
+
+export const TiptapEditor = ({ onChange, content }: TipTapEditorProps) => {
+	// The "Brain"
+	const editor = useEditor({
+		extensions: [StarterKit.configure()],
+		immediatelyRender: false,
+		editorProps: {
+			attributes: {
+				class: "rounded-md border min-h-[150px] border-input bg-background p-4",
+			},
+		},
+		onUpdate({ editor }) {
+			onChange(editor.getHTML());
+		},
+		content: content,
+	});
+
+	return (
+		<div className="flex flex-col gap-2">
+			<Toolbar editor={editor} />
+			<EditorContent editor={editor} />
+		</div>
+	);
+};
--- src/components/Threads/ThreadComments.tsx
+++ src/components/Threads/ThreadComments.tsx
@@ -1,5 +1,9 @@
+"use client";
+import { useState } from "react";
 import { Separator } from "@/components/ui/separator";
 import type { ThreadCommentsType } from "@/lib/Models/BaseModels";
+import { TiptapEditor } from "../RichTextEditor";
+import { Button } from "../ui/button";
 
 interface ThreadCommentsProps {
 	comment_count: number;
@@ -7,8 +11,19 @@ interface ThreadCommentsProps {
 }
 
 const ThreadComments = ({ comments, comment_count }: ThreadCommentsProps) => {
+	const [content, setContent] = useState("");
+
 	return (
 		<div className="mt-20">
+			<div className="flex flex-col gap-4">
+				<TiptapEditor
+					content={content}
+					onChange={(newContent: string) => setContent(newContent)}
+				/>
+				<Button onClick={() => {}} className="self-end">
+					Submit Reply
+				</Button>
+			</div>
 			<Separator className="mt-20" />
 			<h2 className="mt-4 text-xl font-bold">Replies ({comment_count})</h2>
 			{comment_count === 0 && (
