5997a3385de20bad677a692c6cc790469a4478bc
AlexC01 <acuadraq@gmail.com>
2025-09-11 22:20:29 -0400

    feat: adding schema for the formvalidation

---
+++ src/lib/schemas/userSchema.ts
@@ -0,0 +1,14 @@
+import * as z from "zod";
+
+export const authSchema = z.object({
+	email: z.email("Please enter a valid email address"),
+	username: z.string().min(3, "Username must be at least 3 characters long"),
+	password: z
+		.string()
+		.min(6, "Password must be at least 6 characters long")
+		.regex(/[A-Z]/, "Password must contain at least one uppercase letter")
+		.regex(/[a-z]/, "Password must contain at least one lowercase letter")
+		.regex(/[0-9]/, "Password must contain at least one number"),
+});
+
+export type AuthFormData = z.infer<typeof authSchema>;
