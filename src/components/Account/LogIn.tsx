3a42c5d2b574750cedb606a27e8651e182e39d13
AlexC01 <acuadraq@gmail.com>
2025-09-11 22:20:55 -0400

    feat: adding the first steps of the forms for login/signup

--- src/components/Account/LogIn.tsx
+++ src/components/Account/LogIn.tsx
@@ -1,5 +1,63 @@
+"use client";
+
+import { Form, SubmitHandler, useForm } from "react-hook-form";
+import {
+	Card,
+	CardContent,
+	CardDescription,
+	CardFooter,
+	CardHeader,
+	CardTitle,
+} from "@/components/ui/card";
+import { Separator } from "@/components/ui/separator";
+import { AuthFormData, authSchema } from "@/lib/schemas/userSchema";
+import { Button } from "@/components/ui/button";
+import { Input } from "@/components/ui/input";
+import {zodResolver} from '@hookform/resolvers/zod';
+
 const LogIn = () => {
-	return <div>LogIn</div>;
+	const {
+		register,
+		handleSubmit,
+		formState: { errors },
+	} = useForm<AuthFormData>({resolver: zodResolver(authSchema)});
+
+	const onSubmit: SubmitHandler<AuthFormData> = (data) => console.log(data);
+
+	return (
+		<Card className="min-w-xl mt-2 px-8">
+			<CardHeader className="text-center">
+				<CardTitle className="text-3xl">Welcome Back</CardTitle>
+				<CardDescription className="text-muted-foreground mt-1">
+					Enter your email below to login to your account
+				</CardDescription>
+			</CardHeader>
+			<div className="px-12">
+				<Separator />
+			</div>
+			<form onSubmit={handleSubmit(onSubmit)}>
+				<Input
+					{...register("email")}
+					className="mb-4"
+				/>
+				{errors.email && (
+					<p className="text-xs text-red-400" role="alert">
+						{errors.email.message}
+					</p>
+				)}
+				<Input
+					{...register("email")}
+					className="mb-4"
+				/>
+				{errors.email && (
+					<p className="text-xs text-red-400" role="alert">
+						{errors.email.message}
+					</p>
+				)}
+				<Input type="submit" value="Submit" />
+			</form>
+		</Card>
+	);
 };
 
 export default LogIn;
--- src/components/Account/SignUp.tsx
+++ src/components/Account/SignUp.tsx
@@ -1,5 +1,29 @@
+"use client";
+
+import {
+	Card,
+	CardContent,
+	CardDescription,
+	CardFooter,
+	CardHeader,
+	CardTitle,
+} from "@/components/ui/card";
+import { Separator } from "@/components/ui/separator";
+
 const SignUp = () => {
-	return <div>SignUp</div>;
+	return (
+		<Card className="min-w-xl mt-2">
+			<CardHeader className="text-center">
+				<CardTitle className="text-3xl">Create Account</CardTitle>
+				<CardDescription className="text-muted-foreground mt-1">
+					Fill the form below in order to create an account
+				</CardDescription>
+			</CardHeader>
+			<div className="px-12">
+				<Separator />
+			</div>
+		</Card>
+	);
 };
 
 export default SignUp;
