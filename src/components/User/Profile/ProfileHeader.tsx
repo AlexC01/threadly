c5b8aa00edc3226d8f28bc48aac2c12ede2c6c30
AlexC01 <acuadraq@gmail.com>
2025-09-24 22:15:27 -0400

    feat: implementing send message description: adding button to send messages from users profiles

--- src/components/User/Profile/ProfileHeader.tsx
+++ src/components/User/Profile/ProfileHeader.tsx
@@ -1,17 +1,66 @@
+"use client";
 import type { User } from "@supabase/supabase-js";
-import { SquarePen, User as UserIcon } from "lucide-react";
+import {
+	LoaderCircle,
+	MessageSquareText,
+	Send,
+	SquarePen,
+	User as UserIcon,
+} from "lucide-react";
+import { useState } from "react";
+import { toast } from "sonner";
 import TimeAgo from "@/components/TimeAgo";
 import { Button } from "@/components/ui/button";
 import { Card, CardHeader } from "@/components/ui/card";
+import {
+	Popover,
+	PopoverContent,
+	PopoverTrigger,
+} from "@/components/ui/popover";
+import { Textarea } from "@/components/ui/textarea";
 import type { UserStatsType } from "@/lib/Models/BaseModels";
+import { supabase } from "@/lib/supabase/client";
 
 interface ProfileHeaderProps {
-	userInfo: { username: string; created_at: string };
+	userInfo: { username: string; created_at: string; id: string };
 	stats: UserStatsType | null;
 	edit: boolean;
+	currentUser: User | null;
 }
 
-const ProfileHeader = ({ userInfo, stats, edit }: ProfileHeaderProps) => {
+const ProfileHeader = ({
+	userInfo,
+	stats,
+	edit,
+	currentUser,
+}: ProfileHeaderProps) => {
+	const [content, setContent] = useState("");
+	const [loading, setLoading] = useState(false);
+
+	const sendMessage = async () => {
+		if (!currentUser) return;
+		setLoading(true);
+		try {
+			const { data, error } = await supabase.rpc(
+				"create_conversation_and_send_message",
+				{
+					sender_id_input: currentUser.id,
+					receiver_id_input: userInfo.id,
+					message_content: content,
+				},
+			);
+			if (error) throw new Error();
+			console.log(data);
+			setContent("");
+			toast.success("Message sent!");
+		} catch (err) {
+			console.error(err);
+			toast.error("Error while sending message, please try again");
+		} finally {
+			setLoading(false);
+		}
+	};
+
 	return (
 		<div className="flex justify-center w-full items-center px-6 md:px-0">
 			<Card className="w-full max-w-md">
@@ -41,6 +90,35 @@ const ProfileHeader = ({ userInfo, stats, edit }: ProfileHeaderProps) => {
 						<Button size="icon" className="absolute -top-2 right-5">
 							<SquarePen />
 						</Button>
+					)}
+					{!edit && currentUser && (
+						<Popover>
+							<PopoverTrigger asChild>
+								<Button size="icon" className="absolute -top-2 right-5">
+									<MessageSquareText />
+								</Button>
+							</PopoverTrigger>
+							<PopoverContent className="w-72">
+								<Textarea
+									placeholder="Write a message..."
+									value={content}
+									className="resize-none"
+									onChange={(e) => setContent(e.target.value)}
+								/>
+								<div className="justify-end flex">
+									<Button
+										disabled={content === "" || loading}
+										className="mt-3 flex items-center gap-2"
+										size="sm"
+										onClick={sendMessage}
+									>
+										Send
+										{loading && <LoaderCircle className="animate-spin" />}
+										{!loading && <Send />}
+									</Button>
+								</div>
+							</PopoverContent>
+						</Popover>
 					)}
 				</CardHeader>
 			</Card>
